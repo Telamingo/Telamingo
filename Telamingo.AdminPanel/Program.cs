b25e38802dfb53d607b0911e14d9ad70a843f4b2
AmirHosein Goorani <amirhoseingoorani@outlook.com>
2022-04-26 12:59:47 +0430

    fix server bug

--- Telamingo.AdminPanel/Program.cs
+++ Telamingo.AdminPanel/Program.cs
@@ -27,12 +27,12 @@ builder.Services.AddMvc().AddRazorPagesOptions(options =>
 //builder.Services.AddMediatR(typeof(Program));
 builder.Services.AddHttpContextAccessor();
 
-builder.Services.AddMvc()
-    .AddSessionStateTempDataProvider();
-builder.Services.AddHttpClient("AccountClient", c => //Named Http Client
-            {
-    c.DefaultRequestHeaders.Add("X-Custom-Env", "TEST");
-});
+//builder.Services.AddMvc()
+//    .AddSessionStateTempDataProvider();
+//builder.Services.AddHttpClient("AccountClient", c => //Named Http Client
+//            {
+//    c.DefaultRequestHeaders.Add("X-Custom-Env", "TEST");
+//});
 
 //builder.Services.AddAuthentication(x =>
 //{
@@ -158,14 +158,14 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-else
-{
-    app.UseExceptionHandler("./Error");
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseDeveloperExceptionPage();
+//}
+//else
+//{
+//    app.UseExceptionHandler("./Error");
+//}
 
 // Authentication & Authorization
 app.UseAuthentication();
@@ -173,21 +173,14 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
-app.UseEndpoints(endpoints =>
-{
-    //endpoints.MapControllers();
-    endpoints.MapRazorPages();
-});
+//app.UseEndpoints(endpoints =>
+//{
+//    //endpoints.MapControllers();
+//    endpoints.MapRazorPages();
+//});
 //app.UseMvc();
 
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
 //if (app.Environment.IsDevelopment())
 //{
@@ -204,15 +197,6 @@ if (!app.Environment.IsDevelopment())
 //    options.RoutePrefix = string.Empty;
 //});
 
-app.Use(async (context, next) =>
-{
-    context.Response.Headers.Add("Authentication4", "Satinder singh");
-
-    await next.Invoke();
-
-});
-
-app.UseCors(myCorsPolicy);
 
 app.MapRazorPages();
 
