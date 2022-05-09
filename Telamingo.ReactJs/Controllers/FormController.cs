18c4f6f270c239857d5ed0b84a3bb5088c4b4ccf
AmirHosein Goorani <amirhoseingoorani@outlook.com>
2022-05-09 11:39:46 +0430

    <Add/React/Telamingo.js>[Telamingo.sln]: Cleaning

---
+++ Telamingo.ReactJs/Controllers/FormController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Telamingo.ReactJs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormController : ControllerBase
+    {
+        public FormController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("Questions")]
+        public async Task<ActionResult> GetQuestions()
+        {
+
+            return Ok();
+        }
+    }
+}
