4963eadfbdff2338bd2f218ea9d3ed0d795db6fa
fatemeh mafi <f.mafi@takhfifan.com>
2022-05-13 20:30:04 +0430

    import HomePage link in header

--- Telamingo.ReactJs/ClientApp/src/components/layout/header/index.js
+++ Telamingo.ReactJs/ClientApp/src/components/layout/header/index.js
@@ -1,6 +1,7 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
-import logo from "../../../static/header/logo.jpeg"
+import logo from "../../../static/header/logo.jpeg";
 
 import style from "./header.module.css";
 
@@ -11,9 +12,11 @@ const Header = (props) => {
             <div className="container col-lg-12 col-sm-12">
                 <div className={`row ${style.header}`}>
                     <div className={`col-lg-6 col-sm-6 ${style.header_leftSide}`}>
+                        <Link to="/">
                         <img
                             src={logo}
                             alt="logo"/>
+                        </Link>
                         <span>Mentors</span>
                         <span>Blogs</span>
                         <span>About</span>
